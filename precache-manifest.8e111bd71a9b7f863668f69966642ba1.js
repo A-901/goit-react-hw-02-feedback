f36f101d3a4994345355d55b2525305beaf797ef
Alexandr <e7000@ukr.net>
2020-06-01 02:14:28 +0300

    Updates

---
+++ precache-manifest.8e111bd71a9b7f863668f69966642ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b206a7e65f43401566559df35f5b0d8c",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "b1f78fb05410a798cf82",
+    "url": "/goit-react-hw-02-feedback/static/css/main.b9b97a49.chunk.css"
+  },
+  {
+    "revision": "eeb550c76afba7a655c7",
+    "url": "/goit-react-hw-02-feedback/static/js/2.784c1e0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.784c1e0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f78fb05410a798cf82",
+    "url": "/goit-react-hw-02-feedback/static/js/main.2079c41e.chunk.js"
+  },
+  {
+    "revision": "c41a3b7048784827d2cd",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.4a032ff0.js"
+  }
+]);
